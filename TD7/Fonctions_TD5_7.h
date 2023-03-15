471f54fec60e8cded226fa080efc6d3a4191f419
Nicolas PAIVA <nicolas.paiva@limayrac.fr>
2023-03-15 16:36:44 +0100

    update

---
+++ TD7/Fonctions_TD5_7.h
@@ -0,0 +1,215 @@
+﻿#pragma once
+#include <windows.h>
+#include <stdio.h>
+
+// ======================================================================= 
+//   Biblioth�que des fonctions : Fiche TD 5.7
+//		
+// ----------------------------------------------------------------------- 
+//     Ecrit le								par :
+// ----------------------------------------------------------------------- 
+//     Modifi� le							par :
+// ========================================================== data C ===== 
+// ----------------------------------------------------------------------- 
+//     Include                      
+// ----------------------------------------------------------------------- 
+// ----------------------------------------------------------------------- 
+//     define                      
+// ----------------------------------------------------------------------- 
+// ----------------------------------------------------------------------- 
+//     Structure                       
+// ----------------------------------------------------------------------- 
+struct ST_Compte {
+	char NomClient[32];
+	int  Solde;
+	int	 NbreOperations;
+};
+
+// ----------------------------------------------------------------------- 
+//     Prototye des Fonctions 
+// ----------------------------------------------------------------------- 
+void	AfficherCompte(struct ST_Compte*);
+FILE* OpenFichier(char*, char*);
+void	SaisieCompte(struct ST_Compte*);
+
+int		AjoutCompte(char*, struct ST_Compte*, int);
+int		LectureCompte(char*, struct ST_Compte*, int, int);
+int		LireFichierComptes(char*, struct ST_Compte*, int);
+int		ModifieCompte(char*, struct ST_Compte*, int, int);
+// ---- Le dernier int dans ces fonctions corresponde
+//      � une valeur vrai ou faux qui conditionne l'affichage des messages
+
+// ======================================================================= 
+//     Fonction Afficher Compte
+// ======================================================================= 
+void AfficherCompte(struct ST_Compte* p_Compte)
+{
+	printf("\n ----------------------------------------------------------\n");
+	printf("\n\tCLIENT : %s  -  SOLDE =  %d avec %d Operations \n", p_Compte->NomClient, p_Compte->Solde, p_Compte->NbreOperations);
+	printf("\n ----------------------------------------------------------\n");
+}
+
+// ======================================================================= 
+//     Fonction AjoutCompte 
+//								retourne vrai si l'ajout s'est effectu�               
+// ======================================================================= 
+int AjoutCompte(char* NomFichier, struct ST_Compte* p_Compte, int bMessage)
+{
+	FILE* pFichier;
+	bool bOK = true;
+	int NbRetry = 0;
+
+	while (true) {
+		if (bMessage) printf("\nAjout d'un enregistrement au fichier ");
+
+		// ---- Open du Fichier en mode a
+		pFichier = OpenFichier(NomFichier, (char*)"a");
+		if (pFichier == NULL) break;										// On sort en cas d'Erreur de l'open
+
+		// ---- Ecriture de la ligne du compte dans le fichier
+		if (fprintf(pFichier, "%s %d %d\n", p_Compte->NomClient, p_Compte->Solde, p_Compte->NbreOperations) == 0) {
+			printf("\nErreur : Ecriture dans Fichier %s", NomFichier);
+			bOK = false;
+		}
+		else {
+			if (bMessage) printf("\nEcriture dans Fichier OK %s ", NomFichier);
+		}
+		// ---- Close du fichier
+		fclose(pFichier);
+		break;
+	}
+
+	return(bOK);
+}
+
+// ======================================================================= 
+//     Fonction LireFichierComptes                
+// ======================================================================= 
+int LireFichierComptes(char* NomFichier, struct ST_Compte* p_TbCompte, int bMessage)
+{
+	FILE* pFichier;
+	bool bOK = false;
+	struct ST_Compte w_Compte;
+	int NbEnreg = 0;
+
+	while (true) {
+		if (bMessage) printf("\nLecture du fichier des Comptes et mis en tableau");
+
+		// ---- Open du Fichier en mode r
+		pFichier = OpenFichier(NomFichier, (char*)"r");
+		if (pFichier == NULL) break;									// On sort en cas d'Erreur de l'open
+
+		// ---- Lecture du Fichier	ligne par ligne 	
+		while (fscanf(pFichier, "%s %d %d", w_Compte.NomClient, &w_Compte.Solde, &w_Compte.NbreOperations) == 3) {
+			strcpy(p_TbCompte->NomClient, w_Compte.NomClient);
+			p_TbCompte->Solde = w_Compte.Solde;
+			p_TbCompte->NbreOperations = w_Compte.NbreOperations;
+			NbEnreg++;
+			p_TbCompte++;
+		}
+
+		fclose(pFichier);
+		break;
+	}
+	return(NbEnreg);
+}
+// ======================================================================= 
+//     Fonction ModifieCompte dans le fichier 
+// ======================================================================= 
+int ModifieCompte(char* NomFichier, struct ST_Compte* p_Compte, int Index, int bMessage)
+{
+	FILE* pFichier;
+	bool bOK = false;
+	struct ST_Compte w_TbCompte[8];
+	struct ST_Compte* wp_Compte;
+	int NbEnreg = 0;
+
+	while (true) {
+
+		if (bMessage) printf("\nModification du Compte a l'index %d ", Index);
+		// ---- On lit l'integralit� du fichier des comptes	
+		//		et on r�cup�re les Infos des comptes dans un tableau w_TbCompte
+		NbEnreg = LireFichierComptes(NomFichier, w_TbCompte, bMessage);
+
+		// ---- on met a jour le compte w_TbCompte[Index] 
+		//		avec le compte pass� en param�tre p_Compte
+		strcpy(w_TbCompte[Index].NomClient, p_Compte->NomClient);
+		w_TbCompte[Index].Solde = p_Compte->Solde;
+		w_TbCompte[Index].NbreOperations = p_Compte->NbreOperations;
+
+		// ---- On efface le contenu du fichier avec un open en mode w 
+		pFichier = OpenFichier(NomFichier, (char*)"w");
+		if (pFichier == NULL) break;							// On sort en cas d'Erreur de l'open
+		fclose(pFichier);
+
+		// ---- On ecrit tous les enreg du tableau w_TbCompte dans le fichier
+		wp_Compte = w_TbCompte;
+		for (int w_i = 0; w_i < NbEnreg; w_i++) {
+			AjoutCompte(NomFichier, wp_Compte, bMessage);
+			wp_Compte++;
+		}
+
+		bOK = true;
+		//		printf("\n-------------------------\n ");
+		break;
+	}
+	return(bOK);
+}
+// ======================================================================= 
+//     Fonction LectureCompte dans le fichier du compte situ� a l'index               
+// ======================================================================= 
+int LectureCompte(char* NomFichier, struct ST_Compte* p_Compte, int Index, int bMessage)
+{
+	bool bOK = false;
+	struct ST_Compte w_TbCompte[8];
+	int NbEnreg = 0;
+
+	while (true) {
+		if (bMessage) printf("\nLecture du Compte a l'index %d", Index);
+		// ---- On met en tableau tous les comptes du fichier
+		NbEnreg = LireFichierComptes(NomFichier, w_TbCompte, bMessage);
+
+		// ---- on met a jour le compte p_Compte 
+		strcpy(p_Compte->NomClient, w_TbCompte[Index].NomClient);
+		p_Compte->Solde = w_TbCompte[Index].Solde;
+		p_Compte->NbreOperations = w_TbCompte[Index].NbreOperations;
+
+		bOK = true;
+
+		break;
+	}
+	return(bOK);
+}
+// ======================================================================= 
+//     Fonction OpenFichier				Retourne l'ID du fichier ouvert
+// ======================================================================= 
+FILE* OpenFichier(char* NomFichier, char* Mode)
+{
+	FILE* pFichier;
+	int NbRetry = 0;
+
+	// ---- Open du Fichier en mode Mode
+	while (true) {
+		pFichier = fopen(NomFichier, Mode);			// ouverture du fichier
+		// on boucle si l'open ne s'est pas bien pass� car le fopen n accepte pas le partage de fichier
+		// donc on attend si il est occup�
+		if (pFichier != NULL) break;						// On sort de l'open OK
+
+		printf("\nErreur a l'open - retry en cours %s", NomFichier);
+		if (NbRetry == 5) break; 							// On sort de l'open apres 5 echecs
+	}
+	return pFichier;
+}
+// ======================================================================= 
+//     Fonction SaisieCompte                
+// ======================================================================= 
+void SaisieCompte(struct ST_Compte* p_Compte)
+{
+	printf("\nSaisie d'un Nom de client ");
+	scanf("%s", p_Compte->NomClient);
+	printf("\nSaisie du Solde ");
+	scanf("%d", &p_Compte->Solde);
+	printf("\nSaisie du Nbre Operations ");
+	scanf("%d", &p_Compte->NbreOperations);
+	printf("\n-------------------------\n ");
+}
--- TD7/TD7.c
+++ TD7/TD7.c
@@ -1,20 +1,219 @@
-// TD7.c : Ce fichier contient la fonction 'main'. L'exécution du programme commence et se termine à cet endroit.
-//
+// ======================================================================= 
+//   Programme : Etude de Cas Mis a jour Compte bancaire
+//   Version 1 : Execution de deux opérations en LINEAIRE
+//   Version 2 : Execution de deux opérations en PARALLELE
+//				 3 threads exécutés en //  ( main  + 2 threads )
+//	une fonction Operation avec 1 parametre est exécuté par chaque thread 
+// ----------------------------------------------------------------------- 
+//     Ecrit le								par :
+// ----------------------------------------------------------------------- 
+//     Modifié le							par :
+// ========================================================== data C ===== 
+// ----------------------------------------------------------------------- 
+//     Include                      
+// ----------------------------------------------------------------------- 
+#include <windows.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include "Fonctions_TD5_7.h"
 
-#include <iostream>
 
+// ----------------------------------------------------------------------- 
+//     define                      
+// ----------------------------------------------------------------------- 
+// ---- Commandes pour le Préprocessor
+//#define PROGRAMMATION_LINEAIRE 
+#define PROGRAMMATION_PARALLELE
+//#define DEKKER = "DEKKER"
+//#define SEMAPHORE = "SEMAPHORE"
+
+// ----------------------------------------------------------------------- 
+//     Structure                       
+// ----------------------------------------------------------------------- 
+
+// ---- Structure pour passer les paramètres à la fonction Operation
+struct ST_Operation {
+	int MontantOperation;
+	int IndexClient;
+	int bMessage;
+};
+
+
+// ----------------------------------------------------------------------- 
+//     Espace de données Global accessible par tous les threads                    
+// ----------------------------------------------------------------------- 
+char NomFichierBanque[64];
+
+#ifdef DEKKER
+
+#endif
+
+#ifdef SEMAPHORE
+
+#endif
+
+// ----------------------------------------------------------------------- 
+//     Prototye des Fonctions 
+// ----------------------------------------------------------------------- 
+void Operation(struct ST_Operation*);
+
+// ======================================================================= 
+//     Main                      
+// ======================================================================= 
 int main()
 {
-    printf("wold word");
+
+	// ---- Declarations des variables locales	
+	DWORD ThreadId1, ThreadId2;									// necessaire pour la creation d'un thread
+	HANDLE hdl_thread0 = 0;
+	HANDLE hdl_thread1 = 0;
+	struct ST_Operation Operation1, Operation2;
+	struct ST_Compte MonCompte;
+
+	int IndexClientA;
+	int bMessage = false;
+
+	// ---- Initialisations 
+#ifdef DEKKER
+	printf("\n ---------------------------- Version DEKKER ---------------------------- \n");
+
+#endif
+
+#ifdef SEMAPHORE
+	printf("\n ---------------------------- Version SEMAPHORE\n");
+
+#endif
+
+	// ----------------------------------------------------------------------- 
+	//     Traitement Principal : Lancement des Operations à réaliser
+	// ----------------------------------------------------------------------- 
+	while (true) {
+		// ----    Initialisation du fichier                     
+		strcpy(NomFichierBanque, "C:\\Users\\n.paiva\\source\\repos\\TD7\\ProgC_SystemeWindows_TD7\\TD7\\Banque.txt");
+
+		// ---- Lecture du Compte du Client 0 sur lequel on va opérer
+		IndexClientA = 0;
+		LectureCompte(NomFichierBanque, &MonCompte, IndexClientA, bMessage);
+		AfficherCompte(&MonCompte);
+
+		// ---- Initialisation des 2 Opérations a effectuer 
+		Operation1.MontantOperation = 1000;			// on credite de 1000
+		Operation1.IndexClient = IndexClientA;
+		Operation1.bMessage = bMessage;
+
+		Operation2.MontantOperation = -2000;		// on debite de 2000
+		Operation2.IndexClient = IndexClientA;
+		Operation2.bMessage = bMessage;
+
+#ifdef PROGRAMMATION_LINEAIRE
+		// ----------------------------------------------------------------------- 
+		//     On appelle les deux operations a effectuer linéairement                       
+		// ----------------------------------------------------------------------- 
+		printf("---------------- VERSION PROGRAMMATION LINEAIRE --------------------------------\n");
+		Operation(&Operation1);
+
+		Operation(&Operation2);
+#endif
+
+#ifdef PROGRAMMATION_PARALLELE
+
+		// ----------------------------------------------------------------------- 
+		//     Démarrage des opérations en Thread                       
+		// ----------------------------------------------------------------------- 
+		printf("---------------- VERSION PROGRAMMATION PARALLELE ------------------------------\n");
+		// ---- Creation du Thread 1
+		hdl_thread0 = CreateThread(NULL, NULL, (LPTHREAD_START_ROUTINE)Operation, &Operation1, NULL, &ThreadId1);
+		if (hdl_thread0 == NULL)
+		{
+			printf(" le thread 0 n'a pu etre lance\n");
+			break;
+		}
+		// ---- Creation du Thread 2
+		hdl_thread1 = CreateThread(NULL, NULL, (LPTHREAD_START_ROUTINE)Operation, &Operation2, NULL, &ThreadId2);
+		if (hdl_thread1 == NULL)
+		{
+			printf(" le thread 1 n'a pu etre lance\n");
+			break;
+		}
+
+		// ----------------------------------------------------------------------- 
+		//     Attente de la Fin des activités                       
+		// ----------------------------------------------------------------------- 
+		WaitForSingleObject(hdl_thread0, INFINITE);
+		WaitForSingleObject(hdl_thread1, INFINITE);
+
+		printf("\n\n\tLes 2 Threads ont termines leur execution\n\n");
+
+#endif
+
+		// ---- Lecture du Compte du Client 0 
+		LectureCompte(NomFichierBanque, &MonCompte, IndexClientA, bMessage);
+		AfficherCompte(&MonCompte);
+		break;
+	}
+
+	// ---- Fin du Processus maître
+	printf("\n\n\n\n");
+
+	getchar();
+
+	ExitProcess(0);
+
+	return 0;
 }
 
-// Exécuter le programme : Ctrl+F5 ou menu Déboguer > Exécuter sans débogage
-// Déboguer le programme : F5 ou menu Déboguer > Démarrer le débogage
 
-// Astuces pour bien démarrer : 
-//   1. Utilisez la fenêtre Explorateur de solutions pour ajouter des fichiers et les gérer.
-//   2. Utilisez la fenêtre Team Explorer pour vous connecter au contrôle de code source.
-//   3. Utilisez la fenêtre Sortie pour voir la sortie de la génération et d'autres messages.
-//   4. Utilisez la fenêtre Liste d'erreurs pour voir les erreurs.
-//   5. Accédez à Projet > Ajouter un nouvel élément pour créer des fichiers de code, ou à Projet > Ajouter un élément existant pour ajouter des fichiers de code existants au projet.
-//   6. Pour rouvrir ce projet plus tard, accédez à Fichier > Ouvrir > Projet et sélectionnez le fichier .sln.
+// ================================================================================== 
+//     Fonction Operation (Crediter : Montant Positif  ou Debiter : Montant Négatif)               
+// ================================================================================== 
+void Operation(struct ST_Operation* p_Param)
+{
+	struct ST_Compte w_Compte;
+	int w_Montant = 0;
+	char LibOperation[20];
+	char Position[20];
+
+#ifdef DEKKER							//    Initialisation DEKKER
+
+#endif
+
+#ifdef SEMAPHORE						//    Initialisation SEMAPHORES
+
+#endif
+
+	// ******************************************************************************************
+	// ----- Reperer la Section Critique
+	// ----- Encadrer la section Critique de son Prologue et Epilogue pour chaque Version
+	// ******************************************************************************************
+	while (true) {
+		// ---- Mis a jour du libelle de l'opération lancée
+		w_Montant = p_Param->MontantOperation;
+		if (w_Montant < 0) {
+			strcpy(Position, "\t\t\t\t\t\t");				// pour afficher les debits sur la droite de l'ecran
+			strcpy(LibOperation, "Debit");
+			printf("\n%sOperation de %s (%d) lancee pour le client %d - \n", Position, LibOperation, w_Montant, p_Param->IndexClient);
+		}
+		else {
+			strcpy(Position, "");
+			strcpy(LibOperation, "Credit");
+			printf("\n%sOperation de %s (%d) lancee pour le client %d - \n", Position, LibOperation, w_Montant, p_Param->IndexClient);
+		}
+
+		// ---- Lecture des infos actuelles du Compte du Client 
+		if (!(LectureCompte(NomFichierBanque, &w_Compte, p_Param->IndexClient, p_Param->bMessage))) break;
+
+		// ---- Traitement de l'Opération
+		printf("\n%sSolde Depart = %d ----- Calcul en cours...\n", Position, w_Compte.Solde);
+		w_Compte.Solde += w_Montant;
+		w_Compte.NbreOperations++;
+		printf("\n%sNouveau Solde = %d \n", Position, w_Compte.Solde);
+
+		Sleep(2000);	// On genere une E/S en espérant de rendre actif l'autre thread 
+
+		// ---- Ecriture dans le Fichier de Sortie des Infos du Compte Maj
+		if (!(ModifieCompte(NomFichierBanque, &w_Compte, p_Param->IndexClient, p_Param->bMessage))) break;
+
+		printf("\n%sOperation de %s terminee - \n", Position, LibOperation);
+		break;
+	}
+}
